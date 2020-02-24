c70d16b45d4010e3a6149f90801ec7c790ec6e92
Patrick Marschlowitz <p.marschlowitz@gmail.com>
2020-02-23 21:31:46 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,116 @@
+//////////////////////////////////////////////////////////
+//  Conditionals, Arrays, Functions, Loops, and Inputs  //
+//////////////////////////////////////////////////////////
+
+//We are going to build a basic CRUD app (Create, Read, Update, Delete). 
+
+//Step 1: Build something to store our data
+const crud = {
+    data: {},
+    //Step 2: Build Modifiers and Accessors
+    // ******* Implement Create Feature *******
+    counter: 0,
+    create: (info) => {
+        crud.data[`Index ${crud.counter}`] = info;
+        crud.counter += 1;
+    },
+    // ******* Implement Read Feature *******
+    read: (int) => {
+        let result;
+        int === '' ? result = Object.entries(crud.data) : result = crud.data[`Index ${int}`];
+        return result;
+    },
+    // ******* Implement Update Feature *******
+    update: (int, val) => {
+        crud.data[`Index ${int}`] = val
+    },
+    // ******* Implement Delete Feature *******
+    delete: (int) => {
+        delete crud.data[`Index ${int}`]
+    },
+
+    indexReset: () => {
+        crud.counter = 0;
+        const dataKeys = Object.keys(crud.data);
+        dataKeys.forEach((val) => {
+            if (val.charAt(val.length - 1) !== '' + crud.counter) {
+                //need to know how to change object keys at this line, currently changes index.
+                crud.data[`Index ${crud.counter}`] = crud.data[`${val}`];
+                crud.counter += 1;
+            } else {
+                crud.counter += 1
+            }
+        });
+    }
+}
+
+// Step 3: Build A Way to Interact With Our Data
+function runCrud() {
+
+    // ******* Implement Ask For Command Feature *******
+    let choice = prompt("Would you like to 'c'reate,  'r'ead,  'u'pdate, 'd'elete, 'i'ndex reset, or 'e'xit?");
+
+    // ******* Implement Ask For Item Modification Feature *******
+    // Follow-up prompts for CRUD features that require more infomration
+    if (choice === 'c') {
+        let num = prompt('How many items would you like to add?')
+        if (num === '') { num = 1 }
+        for (x = num; x > 0; x--) {
+            let c = prompt('What would you like to add?');
+            c = ` ${c}   `
+            crud.create(c);
+        }
+        runCrud();
+
+    } else if (choice === 'r') {
+        let r = prompt("What would you like to read? You may specify an index or press enter for all info");
+        if (r === 'a') {
+            alert(crud.read(''));
+            runCrud();
+        } else if (r >= 0) {
+            alert(crud.read(r));
+            runCrud();
+        }
+
+    } else if (choice === 'u') {
+        let i = prompt('Please specify an index to update.');
+        let u = prompt(`Please specify what information you would like to add at index ${i}`);
+        crud.update(i, u);
+        alert(crud.read(''));
+        runCrud();
+
+    } else if (choice === 'd') {
+        let d = prompt('Please specify the index of the item to be deleted.');
+        crud.delete(d);
+        alert(crud.read(''));
+        runCrud();
+
+    } else if (choice === null) {
+        alert("To exit program please select 'e'xit in the main menu.")
+        runCrud();
+
+    } else if (choice === 'e') {
+        alert('Have a great day!')
+        return;
+
+    } else if (choice === 'i') {
+        alert(crud.read(''));
+        runCrud();
+        return;
+
+    } else {
+        alert('Please select an option.')
+        runCrud();
+    }
+}
+//Step 4: Run this program
+runCrud();
+
+/*
+    Stretch Goal:
+
+    Congrats on building a CRUD app! Right now it may or may not be very useful.
+    Make this CRUD app's intention more clear by making it specific. For example,
+    make it a To-do app (customize the prompts more) or a Recipe storage app
+    (what are some useful data types you can store in your current data storage?).
+*/
